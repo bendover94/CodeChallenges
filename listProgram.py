a95f15de33ebca93e0f88f34f09eaa6d566082fa
bendover94 <56740228+bendover94@users.noreply.github.com>
2021-10-01 21:22:51 +0200

    doit

--- listProgram.py
+++ listProgram.py
@@ -3,3 +3,5 @@
 # The second parameter will be a string that can be one of the following values: asc, desc, and none.
 # If the second parameter is "asc," then the function should return a list with the numbers in ascending order. 
 # If it's "desc," then the list should be in descending order, and if it's "none," it should return the original list unaltered.
+
+hallo
